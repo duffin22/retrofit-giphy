0538fdaf4fef2c73b39146cd7d7fe7496f6c63dd
Matthew Duffin <matthewtduffin@gmail.com>
2016-08-10 09:20:01 -0700

    finished lab

--- GiphyAPI/app/src/main/java/com/hfad/giphyapi/MainActivity.java
+++ GiphyAPI/app/src/main/java/com/hfad/giphyapi/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -45,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     private final static String API_KEY = "dc6zaTOxFJmzC";
 
     private ImageView immyStill;
+    private ProgressBar proggy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         immyStill = (ImageView) findViewById(R.id.imageView2);
+        proggy = (ProgressBar) findViewById(R.id.progress);
 
         final InputMethodManager inputManager= (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
@@ -63,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                searchButton.setVisibility(View.VISIBLE);
                 String text = searchText.getText().toString();
 
                 searchText.clearFocus();
@@ -89,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
                         FixedHeight fixyStill = gifImage.getFixed_height_still();
                         final String stillUrl = fixyStill.getUrl();
 
-                        Button magicButton = (Button) findViewById(R.id.magicButton);
+                        final Button magicButton = (Button) findViewById(R.id.magicButton);
                         magicButton.setVisibility(View.VISIBLE);
 
                         ImageView immyGif = (ImageView) findViewById(R.id.imageView);
@@ -108,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onClick(View view) {
                                 Toast.makeText(MainActivity.this, "Magic Button Pressed!", Toast.LENGTH_SHORT).show();
-
+                                magicButton.setVisibility(View.GONE);
                                 new ImageProcessingAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,stillUrl);
 
                             }
@@ -153,61 +157,21 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
-//            mProgressBar.setProgress(values[0]);
+            proggy.setProgress(values[0]);
         }
 
         @Override
         protected void onPostExecute(Bitmap bitmap) {
             immyStill.setImageBitmap(bitmap);
-//            mProgressBar.setVisibility(View.INVISIBLE);
-
-//      final int left = mImageView.getLeft();
-//      final int top = mImageView.getTop();
-//
-//      final int[] location = new int[2];
-//      mImageView.getLocationOnScreen(location);
-
-
-//      Log.i("MATT_TEST", "left/top: ("+left+","+top +")");
-
-//            mImageView.setOnTouchListener(new View.OnTouchListener(){
-//                @Override
-//                public boolean onTouch(View v, MotionEvent event){
-//
-//                    Bitmap bitty = ((BitmapDrawable)mImageView.getDrawable()).getBitmap();
-//                    Bitmap mutableBitty = bitty.copy(bitty.getConfig(),true);
-//
-//                    int x = (int)event.getX();
-//                    int y = (int)event.getY();
-//
-//                    int px = mutableBitty.getPixel(x,y);
-//                    int red = Color.red(px);
-//                    int green = Color.green(px);
-//                    int blue = Color.blue(px);
-//                    int avg = (red+blue+green)/3;
-//                    int rev = Color.argb(255,avg,avg,avg);
-//
-//                    for (int i = x; i < x+50 && i <mutableBitty.getWidth(); i++) {
-//                        for(int j = y; j < y+50 && i <mutableBitty.getHeight(); j++){
-//                            mutableBitty.setPixel(i, j, rev);
-//                        }
-//                    }
-//
-//                    mImageView.setImageBitmap(mutableBitty);
-//
-//                    Log.i("MATT-TEST","RGB: ("+red+","+green+","+blue+")");
-//
-//                    return true;
-//                }
-//            });
+            proggy.setVisibility(View.INVISIBLE);
 
         }
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-//            mProgressBar.setVisibility(View.VISIBLE);
-//            mProgressBar.setProgress(0);
+            proggy.setVisibility(View.VISIBLE);
+            proggy.setProgress(0);
         }
 
         private Bitmap invertImageColors(Bitmap bitmap){
@@ -224,8 +188,8 @@ public class MainActivity extends AppCompatActivity {
                     int rev = Color.argb(255,255-red,255-green,255-blue);
                     mutableBitmap.setPixel(i,j,rev);
                 }
-//                int progressVal = Math.round((long) (100*(i/(1.0*mutableBitmap.getWidth()))));
-//                mProgressBar.setProgress(progressVal);
+                int progressVal = Math.round((long) (100*(i/(1.0*mutableBitmap.getWidth()))));
+                proggy.setProgress(progressVal);
             }
             return mutableBitmap;
         }
